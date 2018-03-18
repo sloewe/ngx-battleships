348ad8d5d6478b00e6020243176e6d2c405f1f3c
Sebastian Löwe <sebastian.loewe@zenubu.de>
2018-03-18 11:27:18 +0100

    rename child state to slice

--- src/app/harbour/reducers/harbour.reducer.ts
+++ src/app/harbour/reducers/harbour.reducer.ts
@@ -1,15 +1,15 @@
 import { Action } from '@ngrx/store';
 import { HarbourActions, HarbourActionTypes } from '../actions/harbour.actions';
 
-export interface State {
+export interface Slice {
 
 }
 
-export const initialState: State = {
+export const initialState: Slice = {
 
 };
 
-export function reducer(state = initialState, action: HarbourActions): State {
+export function reducer(state = initialState, action: HarbourActions): Slice {
   switch (action.type) {
 
     case HarbourActionTypes.HarbourAction:
